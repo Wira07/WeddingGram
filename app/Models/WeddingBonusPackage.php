ca151eb1c2ab29f6e17fe46e78df35f8d27aa9bd
wiral <wiralodrasaputra07@gmail.com>
2025-08-26 14:23:55 +0700

    Add Eloquent model features and relationships for BonusPackage, BookingTransaction, City, WeddingBonusPackage, WeddingOrganizer, WeddingPackage, WeddingPhoto, and WeddingTestimonial

--- app/Models/WeddingBonusPackage.php
+++ app/Models/WeddingBonusPackage.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WeddingBonusPackage extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'wedding_package_id',
+        'bonus_package_id',
+    ];
+
+    public function weddingPackage(): BelongsTo
+    {
+        return $this->belongsTo(WeddingPackage::class, 'wedding_package_id');
+    }
+
+    public function bonusPackage(): BelongsTo
+    {
+        return $this->belongsTo(BonusPackage::class, 'bonus_package_id');
+    }
 }
